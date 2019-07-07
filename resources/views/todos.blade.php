6d6e5d221c09638e182a484a404be7e49cc109bb
unknown <bhumirocks4eva@gmail.com>
2019-07-07 23:59:13 +0545

    Update the completed status of todo

--- resources/views/todos.blade.php
+++ resources/views/todos.blade.php
@@ -1,8 +1,9 @@
 @extends('layouts')
 
 @section('content')
+<h2>List of To dos</h2>
   <div class="row" >
-    <div class="col-lg-6 col-lg-offset-3">
+    <div class="col-lg-6 col-lg-offset-3 ">
       <form  action="/create/todo" method="post" >
         {{csrf_field()}}
         <input type="text" class="form-control input-lg" name="todo" placeholder="enter the todo">
@@ -13,6 +14,11 @@
   @foreach($todos as $todo)
     {{$todo->todo}} <a href="{{ route('todo.delete', ['id'=> $todo->id] ) }}" class="btn btn-danger">x</a>
     <a href="{{ route('todo.update', ['id'=> $todo->id] ) }}" class="btn btn-primary btn-xs">Update</a>
+    @if(!$todo->completed)
+      <a href="{{ route('todo.completed', ['id'=>$todo->id])}}" class="btn btn-xs btn-success">Mark as completed</a>
+    @else
+      <span class="text-success">Completed</span>
+    @endif
     <br><hr>
   @endforeach
 @endsection
